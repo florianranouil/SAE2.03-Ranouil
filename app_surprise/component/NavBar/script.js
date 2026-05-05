aa01cd1325588505058f538c9fe04b92f00efd0c
Ranouil Florian <ranouil8@mmi-dev.unilim.fr>
2026-05-05 19:50:10 +0200

    iteration surprise juste nav bar qui bug

--- app_surprise/component/NavBar/script.js
+++ app_surprise/component/NavBar/script.js
@@ -9,10 +9,29 @@ NavBar.format = function (
   hProfil = "C.handlerProfil()"
 ) {
   var html = template;
+
   html = html.replaceAll("{{hHome}}", hHome);
   html = html.replaceAll("{{hProfil}}", hProfil);
+
+  // ✅ LOGO
+  html = html.replaceAll("{{logo}}", "/server/images/logo1.png");
+
   return html;
 };
 
-export { NavBar };
+/* 🔥 AJOUT : gestion du scroll */
+NavBar.initScrollEffect = function () {
+  window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
 
+    if (!navbar) return; // sécurité
+
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+};
+
+export { NavBar };
